fc20b1353afbd9dd4b3058d2a3d32bdf3d0aaf47
Eduard Kachan <eduard.k.kachan@gmail.com>
2014-11-14 23:32:38 -0500

    Rooms, rooms, rooms, rooms, rooms, rooms......
    can create rooms, users can join rooms

--- src/assets/scripts/roomScript.js
+++ src/assets/scripts/roomScript.js
@@ -1,3 +1,65 @@
-/**
- * Created by EdVAard on 14-11-14.
- */
+
+jQuery(function($) {
+    // Create New Socket Connection using Socket.io
+    var socket = io();
+
+    var myID;
+
+    //users id fo identification
+    socket.on('myId', function(playerID){
+        myID = playerID;
+        console.log(myID);
+    });
+
+    //initial submit off all users
+    socket.on('users', function(users){
+        clearUsers('.players');
+        users.players.forEach(function(player) {
+            insertUser(player, '.players');
+        });
+
+        clearUsers('.guest');
+        users.guests.forEach(function(guest) {
+            insertUser(guest, '.guest');
+        });
+    });
+
+    //new player joined
+    socket.on('newPlayer',function(user){
+        insertUser(user, '.players');
+    });
+
+    //new guest joined
+    socket.on('newGuest',function(user){
+        console.log(user);
+        insertUser(user, '.guests');
+    });
+
+    var clearUsers = function(listClass){
+        console.log('clear: ' + listClass);
+        var list = $(listClass);
+        list.empty();
+    };
+
+    var insertUser = function(user,listClass){
+        var list = $(listClass);
+        var item = $('<li>');
+        var name = $('<span>').text(user.name);
+
+        if(user.userID == myID){
+            var nameInput = $('<input>').hide();
+            ready = $('<button>').text('ready up').attr('class', 'ready');
+
+            item.attr('class', 'me')
+                .append(nameInput)
+        }else{
+            ready = $('<span>').text('(Ready)').attr('class', 'ready').hide();
+        }
+
+        item.attr('id', '' + user.userID)
+            .append(name);
+
+        list.append(item);
+    };
+
+});
